482e428e863f4b3a34939b101a1a71fd2453b887
Thuan Le <thuanle@ulab.com>
2017-03-22 03:02:28 +0700

    csv

--- app/js/app.js
+++ app/js/app.js
@@ -6,9 +6,11 @@
 	require('angular-route');
 	require('angular-animate');
 	require('angularfire');
+	require('angular-sanitize');
+	require('ng-csv/build/ng-csv.js');
 
 	var addressCtrl = require ('./addressCtrl.js');
-	var myApp = angular.module('myApp', ['ngRoute', 'ngAnimate', 'firebase']);
+	var myApp = angular.module('myApp', ['ngRoute', 'ngAnimate', 'firebase', 'ngSanitize', 'ngCsv']);
 
 	myApp.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
 
